e7c8cd18fd3af52da9729719601a857565f440e4
wszoltysek <szoltysek.w@gmail.com>
2020-06-02 12:59:44 +0200

    pytest fixtures reformat with comprehensions

--- main_app/tests/conftest.py
+++ main_app/tests/conftest.py
@@ -1,34 +1,24 @@
 import os
 import sys
 import pytest
-
-from main_app.tests.utils import create_fake_genre, create_fake_artist, create_fake_track
+from main_app.tests.utils import *
 
 sys.path.append(os.path.dirname(__file__))
 
 
 @pytest.fixture
 def set_up_genre():
-    data = []
-    for _ in range(5):
-        genre = create_fake_genre()
-        data.append(genre)
+    data = [create_fake_genre() for i in range(5)]
     return data
 
 
 @pytest.fixture
 def set_up_artist():
-    data = []
-    for _ in range(5):
-        artist = create_fake_artist()
-        data.append(artist)
+    data = [create_fake_artist() for i in range(5)]
     return data
 
 
 @pytest.fixture
 def set_up_track():
-    data = []
-    for _ in range(5):
-        track = create_fake_track()
-        data.append(track)
+    data = [create_fake_track() for i in range(5)]
     return data
