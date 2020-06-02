c69dc3090ba1c8c698297eafda84e7274d1fb00f
wszoltysek <szoltysek.w@gmail.com>
2020-06-02 11:44:51 +0200

    test create models update

--- main_app/tests/test_models.py
+++ main_app/tests/test_models.py
@@ -1,45 +1,66 @@
 import pytest
 from main_app.models import *
-from main_app.tests.utils import create_fake_genre, create_fake_artist, create_fake_track
+from main_app.tests.utils import *
+
+
+# TESTS FOR CREATE MODELS:
+
+@pytest.mark.django_db
+def test_create_user():
+    """
+    Should create new user object and save it to database
+    """
+    # Given:
+    users_before = User.objects.count()
+    # When:
+    new_user = create_fake_user()
+    # Then:
+    assert User.objects.count() == users_before + 1
+    assert new_user.pk == 1
+    assert new_user.is_anonymous == False
 
 
 @pytest.mark.django_db
-def test_add_genre(set_up_genre):
+def test_create_genre():
     """
     Should create new genre object and save it to database
     """
     # Given:
-    genre_before = Genre.objects.count()
+    genres_before = Genre.objects.count()
     # When:
     new_genre = create_fake_genre()
     # Then:
-    assert Genre.objects.count() == genre_before + 1
-    assert Genre.objects.count() == 6
+    assert Genre.objects.count() == genres_before + 1
+    assert Genre.objects.count() == 1
+    assert new_genre.pk == 1
+    assert new_genre.user.pk == 2
 
 
 @pytest.mark.django_db
-def test_add_artist(set_up_artist):
+def test_create_artist():
     """
     Should create new artist object and save it to database
     """
     # Given:
-    artist_before = Artist.objects.count()
+    artists_before = Artist.objects.count()
     # When:
     new_artist = create_fake_artist()
     # Then:
-    assert Artist.objects.count() == artist_before + 1
-    assert Artist.objects.count() == 6
+    assert Artist.objects.count() == artists_before + 1
+    assert Artist.objects.count() == 1
+    assert new_artist.pk == 1
 
 
 @pytest.mark.django_db
-def test_add_track(set_up_track):
+def test_create_track():
     """
     Should create new track object and save it to database
     """
     # Given:
-    track_before = Track.objects.count()
+    tracks_before = Track.objects.count()
     # When:
     new_track = create_fake_track()
     # Then:
-    assert Track.objects.count() == track_before + 1
-    assert Track.objects.count() == 6
+    assert Track.objects.count() == tracks_before + 1
+    assert Track.objects.count() == 1
+    assert new_track.pk == 1
