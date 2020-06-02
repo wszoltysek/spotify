aba8f7979cdc1efa95ac294cd4e7f2f660f81e39
wszoltysek <szoltysek.w@gmail.com>
2020-06-02 11:31:54 +0200

    test utils update and bugs fixed

--- main_app/tests/utils.py
+++ main_app/tests/utils.py
@@ -5,11 +5,20 @@ from random import randint
 faker = Faker("pl_PL")
 
 
+def create_fake_user():
+    """
+    Generate new user and saves to database
+    """
+    user = User.objects.create(username=faker.first_name_male())
+    return user
+
+
 def create_fake_genre():
     """
     Generate new genre and saves to database
     """
     new_genre = Genre.objects.create(
+        user=create_fake_user(),
         name=faker.word()
     )
     return new_genre
@@ -19,11 +28,10 @@ def create_fake_artist():
     """
     Generate new artist and saves to database
     """
-    new_genre = create_fake_genre()
     new_artist = Artist.objects.create(
         name=faker.word(),
         description=faker.sentence(),
-        genre=new_genre
+        genre=create_fake_genre()
     )
     return new_artist
 
@@ -32,9 +40,8 @@ def create_fake_track():
     """
     Generate new track and saves to database
     """
-    new_artist = create_fake_artist()
     new_track = Track.objects.create(
-        artist=new_artist,
+        artist=create_fake_artist(),
         title=faker.name(),
         label=faker.word(),
         length=randint(1, 5),
